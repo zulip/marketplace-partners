3eff2be1517cf3322824fc4a21b0f5ae9992928e
Mateusz Mandera <mateusz.mandera@zulip.com>
2025-02-10 22:06:10 +0800

    Use 4 spaces for indentation in interactive_script.sh

--- marketplace_docs/templates/Fabric/interactive_script.sh
+++ marketplace_docs/templates/Fabric/interactive_script.sh
@@ -18,10 +18,9 @@ EOM
 read -r _
 
 while  [ -z "${email}" ] || [ -z "${hostname}" ]; do
-read -r -p "Email address for support and error emails (e.g. ops@example.com): " email
-read -r -p "Hostname (e.g. chat.example.com): " hostname
-echo ""
-
+    read -r -p "Email address for support and error emails (e.g. ops@example.com): " email
+    read -r -p "Hostname (e.g. chat.example.com): " hostname
+    echo ""
 done
 
 cat <<EOM
@@ -36,13 +35,13 @@ sudo service nginx stop
 array=(./zulip-server-*)
 "${array[0]}"/scripts/setup/install --certbot --email="$email" --hostname="$hostname" --no-dist-upgrade
 if [ "$?" = 1 ]; then
-   echo "For troubleshooting see https://zulip.readthedocs.io/en/stable/production/troubleshooting.html."
-   echo -e "\n"
-   echo -e "\e[33mRun the following command or relogin to this server to retry installation\e[0m"
-   echo "  /opt/zulip/interactive_script.sh"
+    echo "For troubleshooting see https://zulip.readthedocs.io/en/stable/production/troubleshooting.html."
+    echo -e "\n"
+    echo -e "\e[33mRun the following command or relogin to this server to retry installation\e[0m"
+    echo "  /opt/zulip/interactive_script.sh"
 else
-   cp -f /etc/skel/.zulip_bashrc /root/.bashrc
-   touch /opt/zulip/.configured
-   # Re-enable the systemd job we disabled in image creation.
-   systemctl enable --quiet apt-daily.timer apt-daily-upgrade.timer
+    cp -f /etc/skel/.zulip_bashrc /root/.bashrc
+    touch /opt/zulip/.configured
+    # Re-enable the systemd job we disabled in image creation.
+    systemctl enable --quiet apt-daily.timer apt-daily-upgrade.timer
 fi
