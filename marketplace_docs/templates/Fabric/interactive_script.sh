ebcf0e045965d40de23435b15d5161abf46ffd79
Mateusz Mandera <mateusz.mandera@zulip.com>
2025-02-10 22:08:45 +0800

    Add prompts to use new installer flags for push notifications
    Starting with Zulip 10.0, there are new installer flags to automate the
    initial setup for push notification service.

--- marketplace_docs/templates/Fabric/interactive_script.sh
+++ marketplace_docs/templates/Fabric/interactive_script.sh
@@ -23,6 +23,38 @@ while  [ -z "${email}" ] || [ -z "${hostname}" ]; do
     echo ""
 done
 
+SERVICES_FLAGS=""
+echo ""
+echo "Would you like to automatically register your server for push notifications?"
+echo "For details on why a centralized push notification service is necessary, see:"
+echo "    <https://zulip.com/doc-permalinks/why-service>"
+read -r -p "Answer [Y/n]: " push_prompt
+case "${push_prompt,,}" in
+    "" | y | yes)
+        SERVICES_FLAGS="--push-notifications"
+
+        echo ""
+        echo "Should your server upload aggregate usage statistics to the push notification service?"
+        echo "For details, see:"
+        echo "    <https://zulip.com/doc-permalinks/usage-statistics>"
+        read -r -p "Answer [Y/n]: " stats_prompt
+        case "${stats_prompt,,}" in
+            n | no)
+                echo "Aggregate usage statistics will not be uploaded."
+                SERVICES_FLAGS="${SERVICES_FLAGS} --no-submit-usage-statistics"
+                ;;
+            *)
+                echo "Agrregate usage statistics will be uploaded."
+                ;;
+        esac
+        ;;
+    *)
+        echo "Push notifications will not be enabled; continuing with installation..."
+        ENABLE_PUSH_NOTIFICATIONS=0
+        ;;
+esac
+sleep 1.5
+
 cat <<EOM
 
 Configuring Zulip. This might take a few minutes.
@@ -33,11 +65,11 @@ EOM
 sudo service nginx stop
 
 array=(./zulip-server-*)
-"${array[0]}"/scripts/setup/install --certbot --email="$email" --hostname="$hostname" --no-dist-upgrade
+"${array[0]}"/scripts/setup/install ${SERVICES_FLAGS} --certbot --email="$email" --hostname="$hostname" --no-dist-upgrade
 if [ "$?" = 1 ]; then
-    echo "For troubleshooting see https://zulip.readthedocs.io/en/stable/production/troubleshooting.html."
+    echo "For troubleshooting, see https://zulip.readthedocs.io/en/stable/production/troubleshooting.html."
     echo -e "\n"
-    echo -e "\e[33mRun the following command or relogin to this server to retry installation\e[0m"
+    echo -e "\e[33mRun the following command or log in to this server again to retry installation\e[0m"
     echo "  /opt/zulip/interactive_script.sh"
 else
     cp -f /etc/skel/.zulip_bashrc /root/.bashrc
